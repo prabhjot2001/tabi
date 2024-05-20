b0078b4dfaea2f5829dcdcdee9a66e963c397367
prabhjot2001 <lazycoder001@gmail.com>
2024-05-20 16:27:59 +0530

    20-5-2024

---
+++ pattern/triangle3.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+/*
+
+     *****
+      ****
+       ***
+        **
+         *
+
+ */
+
+int main()
+{
+    int n = 5;
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= n; j++)
+        {
+            if (i<=j)
+            {
+                cout << "*";
+            }
+            else{
+                cout << " ";
+            }
+        }
+        cout << endl;
+    }
+}
